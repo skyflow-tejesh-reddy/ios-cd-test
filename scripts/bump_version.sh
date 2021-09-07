01ddfc8b4341afe9befcfa5a5be6c0cb15dd3304
Tejesh Reddy <tejesh.allampati@skyflow.com>
2021-09-07 21:10:31 +0530

    workflow fix

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -1,3 +1,3 @@
 echo "Bumping pod version to $1"
 
-sed -r "s/([0-9]+)\.([0-9]+)\.([0-9]+)(-([0-9]+))?/$1/g" "../dummy-pod-for-cd.podspec"
+sed -E "s/([0-9]+)\.([0-9]+)\.([0-9]+)(.([0-9]+))?/$1/g" "./dummy-pod-for-cd.podspec"
