36413f444f77c6df4d1e096078989c45f8bd8f17
Tejesh Reddy <tejesh.allampati@skyflow.com>
2021-09-08 19:27:09 +0530

    dummy scriptupdw

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -3,7 +3,11 @@ echo "Bumping pod version to $1.$2"
 version=$1
 SEMVER=${version:1}
 
-sed -E "s/spec.version .+/spec.version      = \"$SEMVER.$2\"/g" "./dummy-pod-for-cd.podspec" > tempfile
+if [ -z $1]
+then
+    sed -E "s/spec.version .+/spec.version      = \"$SEMVER\"/g" "./dummy-pod-for-cd.podspec" > tempfile
+else
+    sed -E "s/spec.version .+/spec.version      = \"$SEMVER.$2\"/g" "./dummy-pod-for-cd.podspec" > tempfile
 
 sed -E "s/:commit => \".+\"/:commit => \"$2\"/g" tempfile > "./dummy-pod-for-cd.podspec" && rm -f tempfile
 
