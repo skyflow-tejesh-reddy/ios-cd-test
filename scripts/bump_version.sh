d9aa2135c863fbf44db8287166da575d8ebcf507
Tejesh Reddy <tejesh.allampati@skyflow.com>
2021-09-08 09:21:35 +0530

    script fix

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -1,3 +1,7 @@
-echo "Bumping pod version to $1"
+echo "Bumping pod version to v$1"
 
-sed -E "s/([0-9]+)\.([0-9]+)\.([0-9]+)(.([0-9]+))?/$1/g" "./dummy-pod-for-cd.podspec"
+#targetFile = "./dummy-pod-for-cd.podspec"
+
+sed -E "s/([0-9]+)\.([0-9]+)\.([0-9]+)(.([0-9]+))?/$1/g" "./dummy-pod-for-cd.podspec" > tempfile && cat tempfile > "./dummy-pod-for-cd.podspec" && rm -f tempfile
+echo --------------------------
+echo Done, Pod now at v$1
