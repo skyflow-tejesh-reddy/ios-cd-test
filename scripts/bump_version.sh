9258148c508585cd35b60fc238d56747d0bd52e0
Tejesh Reddy <tejesh.allampati@skyflow.com>
2021-09-08 10:32:37 +0530

    fixed version bump1

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -1,7 +1,7 @@
 echo "Bumping pod version to v$1"
 
 
-sed -E "s/([0-9]+)\.([0-9]+)\.([0-9]+)(.(\.+))?/$1.$2/g" "./dummy-pod-for-cd.podspec" > tempfile
+sed -E "s/([0-9]+)\.([0-9]+)\.([0-9]+)(.(\.+))/$1.$2/g" "./dummy-pod-for-cd.podspec" > tempfile
 
 sed -E "s/:commit => \".+\"/:commit => \"$2\"/g" tempfile > "./dummy-pod-for-cd.podspec" && rm -f tempfile
 
