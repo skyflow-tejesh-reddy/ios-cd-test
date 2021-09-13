9d1e920ea1ebbda8487ba2410ac6bf60a5bd5bca
Tejesh Reddy <tejesh.allampati@skyflow.com>
2021-09-13 15:43:25 +0530

    updated echos

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -6,12 +6,17 @@ SEMVER=${version:1}
 if [ -z $2 ]
 then
     sed -E "s/spec.version .+/spec.version      = \"$SEMVER\"/g" "./dummy-pod-for-cd.podspec" > tempfile
+    
+    echo --------------------------
+    echo "Done, Pod now at v$1"
+
 else
     sed -E "s/spec.version .+/spec.version      = \"$SEMVER-dev.$2\"/g" "./dummy-pod-for-cd.podspec" > tempfile
+    
+    echo --------------------------
+    echo "Done, Pod now at v$1-dev.$2"
+
 fi
 
 sed -E "s/:commit => \".+\"/:commit => \"$2\"/g" tempfile > "./dummy-pod-for-cd.podspec" && rm -f tempfile
 
-
-echo --------------------------
-echo "Done, Pod now at $1.$2"
